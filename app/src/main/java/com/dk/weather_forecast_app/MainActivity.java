a1c9d4a4efdb754a80c6f4dd6dbb56c7b7f0a134
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-10-23 01:13:15 +0200

    Populate rest of the UI Views

--- app/src/main/java/com/dk/weather_forecast_app/MainActivity.java
+++ app/src/main/java/com/dk/weather_forecast_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.dk.weather_forecast_app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -12,13 +13,20 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tempTextView;
+    TextView dateTextView;
+    TextView weatherDescriptionTextView;
+    TextView cityTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +34,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tempTextView = (TextView) findViewById(R.id.temperatureTextView);
-        tempTextView.setText("30");
+        dateTextView = (TextView) findViewById(R.id.dateTextView);
+        weatherDescriptionTextView = (TextView) findViewById(R.id.weatherDescriptionTextView);
+        cityTextView = (TextView) findViewById(R.id.cityTextView);
+
+        dateTextView.setText(getCurrentDate());
 
         String url = "http://api.openweathermap.org/data/2.5/weather?q=Warszawa,pl&appid=d6ceb79f9eb5adf3bce194ec5bef2db4&units=metric";
 
@@ -34,14 +46,24 @@ public class MainActivity extends AppCompatActivity {
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
 
                     @Override
-                    public void onResponse(JSONObject response) {
+                    public void onResponse(JSONObject responseObject) {
                         //tempTextView.setText("Response: " + response.toString());
-                        Log.v("WEATHER", "Response: " + response.toString());
+                        //Log.v("WEATHER", "Response: " + responseObject.toString());
 
                         try {
-                            JSONObject mainJSONObject = response.getJSONObject("main");
+                            JSONObject mainJSONObject = responseObject.getJSONObject("main");
+                            JSONArray weatherArray = responseObject.getJSONArray("weather");
+                            JSONObject firstWeatherObject = weatherArray.getJSONObject(0);
+
                             String temp = Integer.toString((int) Math.round(mainJSONObject.getDouble("temp")));
+                            String weatherDescription = firstWeatherObject.getString("description");
+                            String city = responseObject.getString("name");
+
                             tempTextView.setText(temp);
+                            weatherDescriptionTextView.setText(weatherDescription);
+                            cityTextView.setText(city);
+
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -57,5 +79,14 @@ public class MainActivity extends AppCompatActivity {
 
         RequestQueue queue = Volley.newRequestQueue(this);
         queue.add(jsObjRequest);
+    }
+
+    private String getCurrentDate ()
+    {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, MMM dd");
+        String formattedDate = dateFormat.format(calendar.getTime());
+
+        return formattedDate;
     }
 }
