b363e4bd65f6aa57df4a510bf9eb27e4a7fb93ef
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-10-22 12:31:20 +0200

    Get Weather Data From An API

--- app/src/main/java/com/dk/weather_forecast_app/MainActivity.java
+++ app/src/main/java/com/dk/weather_forecast_app/MainActivity.java
@@ -2,12 +2,60 @@ package com.dk.weather_forecast_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tempTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tempTextView = (TextView) findViewById(R.id.temperatureTextView);
+        tempTextView.setText("30");
+
+        String url = "http://api.openweathermap.org/data/2.5/weather?q=Warszawa,pl&appid=d6ceb79f9eb5adf3bce194ec5bef2db4";
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //tempTextView.setText("Response: " + response.toString());
+                        Log.v("WEATHER", "Response: " + response.toString());
+
+                        try {
+                            JSONObject mainJSONObject = response.getJSONObject("main");
+                            String temp = Integer.toString((int) Math.round(mainJSONObject.getDouble("temp")));
+                            tempTextView.setText(temp);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+
+                    }
+                });
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(jsObjRequest);
     }
 }
